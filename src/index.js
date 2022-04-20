42a7d9152b19b14d13ebe60a77382a970052d2e8
Svyatoslav Olshanetskiy <olshanechky@gmail.com>
2022-04-21 00:51:11 +0300

    add pure function

--- src/index.js
+++ src/index.js
@@ -40,17 +40,27 @@ const App = (params) => {
 /////
     //function composition or piping
     const x = 2
+    // let y = 6
+    let y = 6 //it will be param for pur
     const double = (number) => number * 2
     const square = (number) => number * number
-    const half = (number) => number / 2
+    const half = (number) => number / y
+
+    //not a pure function
+    console.log(half(12))//2
+    y = 2
+    console.log(half(12))//6
+
     // const divide = (num1, num2) => num1 / num2
+
+    //carrying
     const divide = (num2) => {
-       return function (num1) {
-        return    num1 / num2
-       }
+        return function (num1) {
+            return num1 / num2
+        }
     }
     // const mathCalculate = compose(half, square, double)
-    const mathCalculate = pipe(double, square, half,divide(3))
+    const mathCalculate = pipe(double, square, half, divide(3))
     // return <h1>{half(square(double(x)))}</h1>
     return <h1>{mathCalculate(x)}</h1>
 
