1b675c09dabff01969106fe104d914062b56add4
Svyatoslav Olshanetskiy <olshanechky@gmail.com>
2022-04-21 00:38:20 +0300

    add carrying

--- src/index.js
+++ src/index.js
@@ -43,8 +43,14 @@ const App = (params) => {
     const double = (number) => number * 2
     const square = (number) => number * number
     const half = (number) => number / 2
+    // const divide = (num1, num2) => num1 / num2
+    const divide = (num2) => {
+       return function (num1) {
+        return    num1 / num2
+       }
+    }
     // const mathCalculate = compose(half, square, double)
-    const mathCalculate = pipe( double,square,half)
+    const mathCalculate = pipe(double, square, half,divide(3))
     // return <h1>{half(square(double(x)))}</h1>
     return <h1>{mathCalculate(x)}</h1>
 
