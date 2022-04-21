4a7e02dc795c095c909a3979f417ad40681943b3
Svyatoslav Olshanetskiy <olshanechky@gmail.com>
2022-04-22 00:06:40 +0300

    add dispatch

--- src/index.js
+++ src/index.js
@@ -8,15 +8,28 @@ function createStore(initialState) {
         return state
     }
 
-    return {getState}
+    function dispatch(action) {
+        // console.log(action)
+        if (action.type === "task/completed") {
+            const newArray = [...state]
+            const elementIndex = newArray.findIndex(el => el.id === action.payload.id)
+            newArray[elementIndex].completed = true
+            state = newArray
+            console.log(state)
+        }
+    }
+
+    return {getState, dispatch}
 }
 
 const store = createStore([{id: 1, description: "Task 1", completed: false}])
 
 const App = (params) => {
     console.log(store.getState())
-
-    return <h1>App</h1>
+    // store.dispatch({type: "task/completed", payload: {id: 1}})
+    return <><h1>App</h1>
+        <button onClick={() => store.dispatch({type: "task/completed", payload: {id: 1}})}>Completed</button>
+    </>
 
 
 }
