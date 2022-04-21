f05a239a2f59836e854c5cbe0c4e21cd98708a87
Svyatoslav Olshanetskiy <olshanechky@gmail.com>
2022-04-21 15:23:24 +0300

    add immutable object

--- src/index.js
+++ src/index.js
@@ -1,68 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import {compose, pipe} from "lodash/fp"
 
 const App = (params) => {
-    //functions as params for other functions
-    // function fn() {
-    //     return "app"
-    // }
-    // function someFn(func){
-    //     return func()
-    // }
-    // return <h1>{someFn(fn)}</h1>
-
-    //functions as variables
-    // function someFn() {
-    //     return function () {
-    //         return "app"
-    //     }
-    // }
-    // const fn = someFn()
-    // return <h1>{fn()}</h1>
-
-    //
-    // function someFn() {
-    //     return function () {
-    //         return "app"
-    //     }
-    // }
-    // function fn(func) {
-    //     return func()
-    // }
-    // return <h1>{fn(someFn())}</h1>
-//////
-//     const arr = [" some", " new", "  data"]
-//     function formatArray(el) {
-//         return el + " some"
-//     }
-//     return <h1>{arr.map(formatArray)}</h1>
-/////
-    //function composition or piping
-    const x = 2
-    // let y = 6
-    let y = 6 //it will be param for pur
-    const double = (number) => number * 2
-    const square = (number) => number * number
-    const half = (number) => number / y
-
-    //not a pure function
-    console.log(half(12))//2
-    y = 2
-    console.log(half(12))//6
-
-    // const divide = (num1, num2) => num1 / num2
-
-    //carrying
-    const divide = (num2) => {
-        return function (num1) {
-            return num1 / num2
-        }
-    }
-    // const mathCalculate = compose(half, square, double)
-    const mathCalculate = pipe(double, square, half, divide(3))
-    // return <h1>{half(square(double(x)))}</h1>
-    return <h1>{mathCalculate(x)}</h1>
+    const [state, setState] = useState({})
+    // setState((prevState)=>({...prevState,id:"12131"}))
+    const obj1 = {id: 2, name: "some name", author: {name: "some name"}}
+    const obj2 = {...obj1, author: {...obj1.author}}//it will be new obj
+    console.log(obj1.author === obj2.author)
+    return <h1>App</h1>
 
 
 }
