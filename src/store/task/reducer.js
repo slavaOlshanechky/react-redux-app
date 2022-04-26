fe5ca425bbaf9ef7b0db192ce219da33c764812a
Svyatoslav Olshanetskiy <olshanechky@gmail.com>
2022-04-26 16:17:17 +0300

    add createReducer

--- src/store/task/reducer.js
+++ src/store/task/reducer.js
@@ -1,19 +1,19 @@
-import {taskUpdated, taskDeleted} from "./actionTypes";
-
-export function reducer(state = [], action) {
-    switch (action.type) {
-        case taskUpdated: {
-            const newArray = [...state]
-            const elementIndex = newArray.findIndex(
-                el => el.id === action.payload.id
-            )
-            newArray[elementIndex] = {...newArray[elementIndex], ...action.payload}
-            return newArray
-        }
-        case taskDeleted: {
-            return state.filter((el) => el.id !== action.payload.id)
-        }
-        default:
-            return state;
-    }
-}
+// import {taskUpdated, taskDeleted} from "./actionTypes";
+//
+// export function reducer(state = [], action) {
+//     switch (action.type) {
+//         case taskUpdated: {
+//             const newArray = [...state]
+//             const elementIndex = newArray.findIndex(
+//                 el => el.id === action.payload.id
+//             )
+//             newArray[elementIndex] = {...newArray[elementIndex], ...action.payload}
+//             return newArray
+//         }
+//         case taskDeleted: {
+//             return state.filter((el) => el.id !== action.payload.id)
+//         }
+//         default:
+//             return state;
+//     }
+// }
