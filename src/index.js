46625247ae502ccdb095b905e5925a337daa710c
Svyatoslav Olshanetskiy <olshanechky@gmail.com>
2022-05-01 14:43:00 +0300

    add isLoading and error handling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
 import createStore from "./store/store";
 import {completeTask, getTasks, taskDeleted, titleChanged} from "./store/task";
@@ -8,10 +8,12 @@ import {Provider, useDispatch, useSelector} from "react-redux";
 let store = createStore()
 
 const App = (params) => {
-    const state = useSelector((state) => state)
-const dispatch=useDispatch()
+    const state = useSelector((state) => state.entities)
+    const isLoading = useSelector((state) => state.isLoading)
+    const error = useSelector((state) => state.error)
+    const dispatch = useDispatch()
     useEffect(() => {
-       dispatch(getTasks())
+        dispatch(getTasks())
 
         // console.log("hello")
         // return () => {
@@ -21,12 +23,17 @@ const dispatch=useDispatch()
     }, [])
 
     const changeTitle = (taskId) => {
-       dispatch(titleChanged(taskId))
+        dispatch(titleChanged(taskId))
     }
     const deleteTask = (taskId) => {
-       dispatch(taskDeleted(taskId))
+        dispatch(taskDeleted(taskId))
     }
-
+    if (isLoading) {
+        return <h1>Loading...</h1>
+    }
+    if(error){
+        return <p>{error}</p>
+    }
     return (<><h1>App</h1>
             <ul>
                 {state.map((el) => (
